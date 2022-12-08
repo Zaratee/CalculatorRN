5faaeb837d50394f17c849be7989437ab746a7c3
Zaratee <zarate251098@hotmail.com>
2022-12-07 18:33:15 -0600

    App init
    App cli setup

---
+++ Calculator/src/screens/CalculatorScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const CalculatorScreen = () => {
+    return (
+        <View style={{
+            flex: 1,
+            padding: 40,
+            backgroundColor: 'red'
+        }}>
+            <Text style={{
+                fontSize: 30
+            }}>
+                bns
+            </Text>
+        </View>
+    )
+}
